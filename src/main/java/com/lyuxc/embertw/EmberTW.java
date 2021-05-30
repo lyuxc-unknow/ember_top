542ccdaa6dccb0de8063374128790a0bf1647666
lyuxc-unknow <2675796669@qq.com>
2021-05-30 10:15:56 +0800

    修改

--- src/main/java/com/lyuxc/embertw/EmberTW.java
+++ src/main/java/com/lyuxc/embertw/EmberTW.java
@@ -14,7 +14,7 @@ import java.io.File;
 @Mod(
         modid = EmberTW.MOD_ID,
         name = EmberTW.MOD_NAME,
-        version = "0.0.4",
+        version = "0.0.10",
         dependencies = "required-after:embers;after:theoneprobe;after:waila"
 )
 
